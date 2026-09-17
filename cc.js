f5ade567710588da44dafe6ab27beac5bbc17322
Josan866 <joshua.millner@online.de>
2026-09-17 14:13:24 +0200

    Update cc.js

--- cc.js
+++ cc.js
@@ -1,1 +1,1 @@
-document.getElementById('cc').innerHTML = `© ${new Date().getFullYear()} JsCreations`
+document.getElementById('cc').innerHTML = `Stand: September 2026 • © ${new Date().getFullYear()} JsCreations`
